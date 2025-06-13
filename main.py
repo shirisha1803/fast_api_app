2ba332dba06aed3b59a91918294acc4e6cde1774
shirisha1803 <shirisha1832@gmail.com>
2025-06-13 21:54:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
-from .api import router as api_router
+from api import router as api_router
 
 # Initialize the rate limiter
 limiter = Limiter(key_func=get_remote_address, default_limits=["10/minute"])
